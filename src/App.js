5de4132497cadc8be344ac569cc044d4cb13bbb7
aso <titechei@gmail.com>
2019-09-05 12:38:35 +0900

    Create the editor pane

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import SplitPane from 'react-split-pane';
 import logo from './logo.svg';
+import Editor from './editor.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <SplitPane split="vertical" defaultSize="50%">
-        <div className="editor-pane"></div>
-        <div className="view-pane"></div>
-      </SplitPane>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super();
+    this.state = {
+      markdownSrc: "# Hello World"
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <SplitPane split="vertical" defaultSize="50%">
+          <div className="editor-pane">
+            <Editor className="editor" value={this.state.markdownSrc}/>
+          </div>
+          <div className="view-pane"></div>
+        </SplitPane>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/editor.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import CodeMirror from '@skidding/react-codemirror';
+require('codemirror/lib/codemirror.css');
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/python/python');
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/markdown/markdown');
+require('codemirror/theme/monokai.css');
+
+class Editor extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const options = {
+      mode: 'markdown',
+      theme: 'monokai'
+    }
+    return (
+      <CodeMirror value={this.props.value} options={options} height="100%"/>
+    );
+  }
+}
+
+export default Editor;
