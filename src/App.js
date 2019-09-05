eb4c9f316cbad8f37b5a9ca48a0783afabf44e6e
aso <titechei@gmail.com>
2019-09-05 12:02:27 +0900

    Split pane

--- src/App.js
+++ src/App.js
@@ -6,20 +6,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SplitPane split="vertical" defaultSize="50%">
+        <div className="editor-pane"></div>
+        <div className="view-pane"></div>
+      </SplitPane>
     </div>
   );
 }
